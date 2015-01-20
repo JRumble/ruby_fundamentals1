6b90bb4f24181462f8c059945c62f4384b830d8d
JRumble <jody.paget@rogers.com>
2015-01-20 16:19:57 -0500

    completed exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,15 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+
+require 'date'
+
+puts "How old are you?"
+age = gets.chomp
+
+
+
+year = Date.today.year - age.to_i - 1
+
+puts "You must have been born in #{year}."
